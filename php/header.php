ba156aa5119390e02638ea0c6242384a6d169122
Sergio Pascual <sergiopascual96@gmail.com>
2022-03-29 20:41:27 +0200

    añadido todo el php
    He organizado y añadido todos los php para hacer la estructura del html

---
+++ php/header.php
@@ -0,0 +1,14 @@
+<header class="main-header">
+	<div class="btns-header">
+		<button class="btn-main btn-main--blue">Acceso candidatos</button>
+		<button class="btn-main btn-main--blue">Acceso empresas</button>
+	</div>
+    <section class="logo-header">
+        <div class="logotipo-header">
+            <a href="">
+                <img class="logotipo-principal" src="/images/Logotipo-principal.svg" alt="logotipo ESAT jobs">
+            </a>
+        </div>
+        <h2 class="mobile-subtitle">Impulsa tu carrera trabajando en una empresa líder</h2>
+    </section>
+</header>
