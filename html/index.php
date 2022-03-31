015b898cb6d499cf7a32c841b6fe75976bcdb85b
Sergio Pascual <sergiopascual96@gmail.com>
2022-03-31 14:51:58 +0200

    añadido header a todas las páginas
    He añadido el header a todas las páginas y he comenzado a darle un estilo

---
+++ html/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include('../php/head.php');
+    ?>
+    <title>Smooth</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<?php
+    include('../php/header.php');
+?>
+<?php
+    include('../php/inicio/home.php');
+?>
+<?php
+    include('../php/footer.php');
+?>
+
+
+
+</body>
+</html>
---
+++ html/realizar-pedido.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include('../php/head.php');
+    ?>
+    <title>Realizar Pedido</title>
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<?php
+    include('../php/header.php');
+?>
+<?php
+    include('../php/tienda/realizar-pedido.php');
+?>
+<?php
+    include('../php/footer.php');
+?>
+
+
+
+
+</body>
+</html>
