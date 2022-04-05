ff572f6f03849c81cd4564485db553b0525439fd
AlexDasi <sergiopascual96@gmail.com>
2022-04-05 20:46:24 +0200

    boton borrar
    boton borrar

---
+++ src/basura.js
@@ -0,0 +1,9 @@
+const desactivado = 'desactivado';
+// const añadir = document.querySelector('.').classList.add('desactivado');
+
+function myFunction() {
+    var element = document.getElementById("borrar");
+    element.classList.add('desactivado');
+    document.getElementById('1').classList.add('desactivado');
+    document.getElementById('1').classList.add('desactivado');
+}
